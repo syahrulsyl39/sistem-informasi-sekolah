1df09a994672e842e5739802e54f3816350996dc
syahrulsyl39 <syahrulsaleh39@gmail.com>
2022-04-21 11:51:58 +0700

    menambahkan file model siswa

---
+++ database/migrations/2022_04_21_045040_create_siswas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSiswasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('siswas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_depan');
+            $table->string('nama_belakang');
+            $table->string('jenis_kelamin');
+            $table->string('agama');
+            $table->string('alamat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('siswas');
+    }
+}
